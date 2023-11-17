b9bee214ebfdfc00c8ec38a23bb69cfacb9528f3
dliang <humgal@outlook.com>
2023-11-17 18:13:46 +0800

    title

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'SimulatedWorld',
   author: 'SimulatedWorld',
   headerTitle: 'Simulated World',
   description: '',
